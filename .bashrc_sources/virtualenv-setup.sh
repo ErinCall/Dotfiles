90f95395b62810530a1c3c0171e6f556130e96fb
Andrew Lorente <andrew.lorente@gmail.com>
2011-06-01 18:34:46 -0700

    Don't spit errors when virtualenv isn't set up

--- .bashrc_sources/virtualenv-setup.sh
+++ .bashrc_sources/virtualenv-setup.sh
@@ -1,10 +1,12 @@
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
-export WORKON_HOME=~/Envs
-export PIP_VIRTUALENV_BASE=$WORKON_HOME
-export PIP_RESPECT_VIRTUALENV=true
-export PATH=/usr/local/share/python/:$PATH
+if [ -s /usr/local/share/python/virtualenvwrapper.sh ]; then
+    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+    export WORKON_HOME=~/Envs
+    export PIP_VIRTUALENV_BASE=$WORKON_HOME
+    export PIP_RESPECT_VIRTUALENV=true
+    export PATH=/usr/local/share/python/:$PATH
 
-source /usr/local/share/python/virtualenvwrapper.sh
+    source /usr/local/share/python/virtualenvwrapper.sh
+fi
 
 function current_virtualenv {
     env=`echo $VIRTUAL_ENV | ack -o '\w+$'`
