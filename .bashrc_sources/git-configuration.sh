48e9f3d0ea943dd1f6ec20c7cd7dc91e5cdac66f
Andrew Lorente <andrew.lorente@gmail.com>
2011-12-05 12:59:04 -0800

    use git_expand_args in gap()
    This creates a very terse shortcut for adding -p a single file

--- .bashrc_sources/git-configuration.sh
+++ .bashrc_sources/git-configuration.sh
@@ -4,17 +4,20 @@ function grugrhom {
     MASTER=${1:-origin/master}
     git remote update && git reset --hard $MASTER
 }
+
 function glorm {
     MASTER=${1:-master}
     git log --oneline --reverse $MASTER..
 }
 alias glorom='glorm origin/master'
+
 function grieve {
     MASTER=${1:-master}
     git log -p --reverse --stat --no-prefix $MASTER..
 }
 alias grieveom='grieve origin/master'
+
 function gap {
     path=${1:-.}
-    git add -p $path
+    git add -p `git_expand_args $path`
 }
