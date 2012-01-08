4538b9656def9c64dacf49572b94a3f6353d5804
Andrew Lorente <andrew.lorente@gmail.com>
2012-01-08 12:09:50 -0800

    Move 'rvm use' into local.sh

--- .bashrc_sources/rvm-setup.sh
+++ .bashrc_sources/rvm-setup.sh
@@ -1,7 +1,3 @@
-if [ ! $GEM_HOME ]; then
-    which rvm &>/dev/null && rvm use 1.9.2
-fi
-
 function current_gemset {
     gemset=`echo $GEM_HOME | awk -F '@' '{print $2}'`
     if [ $gemset ]; then
