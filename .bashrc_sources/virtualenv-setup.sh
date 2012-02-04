5f72b7ed5c9bdc5e88a55be576e617157bdf2b96
Andrew Lorente <andrew.lorente@gmail.com>
2012-02-04 06:59:42 -0800

    virtualenvwrapper lives in bin, not share
    It'd been in share in one crappy setup I had but it typically goes in
    bin.

--- .bashrc_sources/virtualenv-setup.sh
+++ .bashrc_sources/virtualenv-setup.sh
@@ -1,11 +1,11 @@
-if [ -s /usr/local/share/python/virtualenvwrapper.sh ]; then
+if [ -s /usr/local/bin/virtualenvwrapper.sh ]; then
     export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
     export WORKON_HOME=~/Envs
     export PIP_VIRTUALENV_BASE=$WORKON_HOME
     export PIP_RESPECT_VIRTUALENV=true
     export PATH=/usr/local/share/python/:$PATH
 
-    source /usr/local/share/python/virtualenvwrapper.sh
+    source /usr/local/bin/virtualenvwrapper.sh
 fi
 
 function current_virtualenv {
