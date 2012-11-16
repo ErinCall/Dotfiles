5f6957ccf3d550a6e17e44ecafc371e75917ee48
Andrew Lorente <alorente@janrain.com>
2012-11-16 08:56:28 -0800

    "Git checkout ticket" function

--- .bashrc_sources/git-configuration.sh
+++ .bashrc_sources/git-configuration.sh
@@ -26,3 +26,19 @@ function gap {
     path=${1:-.}
     git add -p `git_expand_args $path`
 }
+
+function gcot {
+    ticket=$1
+    if [ $ticket ]; then
+      branch=`git branch | sed s/.// | ack $ticket`
+      if [ $branch ]; then
+        git checkout $branch
+      else
+        echo "no branch found for $ticket"
+        return 2
+      fi
+    else
+      echo "You need to give me a ticket"
+      return 1
+    fi
+}
