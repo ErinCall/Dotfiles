4549fd507f1aaa9ec5bda8e7848baf9ca3e448e0
Erin Call <hello@erincall.com>
2015-06-09 14:04:18 -0700

    Compatibility fix for SCM Breeze
    After updating SCM Breeze (f0e78d8f7b119a6936de292a6d91b783da57a201) the
    helper variables I was using are no longer available. It's fine; I'm not
    likely to mess around with the command names I want.

--- .bashrc_sources/scm_breeze-configuration.sh
+++ .bashrc_sources/scm_breeze-configuration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-alias $git_diff_alias="$_exp git dif"
-alias $git_diff_cached_alias="$_exp git dif --cached"
+alias gd="exec_git_expand_args git dif"
+alias gdc="exec_git_expand_args git dif --cached"
