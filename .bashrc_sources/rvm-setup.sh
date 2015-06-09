feea0db9404aff2261d2933bab74c1f7782bf860
Erin Call <hello@erincall.com>
2015-06-09 14:03:04 -0700

    Fix scm-breeze/rvm conflict
    SCM Breeze aliases cd, which breaks rvm, so it's supposed to be loaded
    after rvm. I can't do that, though, because rvm doesn't load until I say
    do_ruby. Instead, unalias cd and then re-load scm breeze.

--- .bashrc_sources/rvm-setup.sh
+++ .bashrc_sources/rvm-setup.sh
@@ -1,8 +1,12 @@
 export DEFAULT_RVM_RUBY=2.1.2
 [[ -d ~/.rvm/bin ]] && export PATH=$PATH:~/.rvm/bin
 function do_ruby {
+    if [ -n "$(alias cd 2>/dev/null)" ]; then
+        unalias cd
+    fi
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
     rvm use $DEFAULT_RVM_RUBY
+    . ~/.bashrc_sources/externals/scm_breeze/scm_breeze.sh
 }
 
 function current_gemset {
