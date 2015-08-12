3196e61ebce17fde7f067062faeadb051357ff16
Erin Call <hello@erincall.com>
2015-08-12 15:06:20 -0700

    Change git-prompt status symbols
    I think these look nicer! The wider unicode symbols don't display
    wonderfully, at least in Monaco, hence the trailing spaces.

--- .config/fish/functions/git_prompt.fish
+++ .config/fish/functions/git_prompt.fish
@@ -40,15 +40,22 @@ function git_prompt
         end
     else if [ (git rev-parse --is-inside-work-tree ^/dev/null) = "true" ]
         git diff --no-ext-diff --ignore-submodules \
-                 --quiet --exit-code; or set outstanding '*'
+                 --quiet --exit-code; or set outstanding '◯ '
         if git rev-parse --quiet --verify HEAD >/dev/null ^&1
             git diff-index --cached --quiet \
-                           --ignore-submodules HEAD --; or set head '+'
+                           --ignore-submodules HEAD --; or set head '◉ '
         else
             set head '#'
         end
     end
 
-    echo -n '('$bare$branch$outstanding$head$rebasing')'
+    echo -n '('$bare$branch
+    if begin
+        [ $outstanding ]
+        or [ $head ]
+    end
+        echo -n ' '
+    end
+    echo $head$outstanding$rebasing')'
 end
 
