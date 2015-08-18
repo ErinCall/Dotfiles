ff9fec46c0802517fbcdce45f65fa1540db73abb
Erin Call <hello@erincall.com>
2015-08-18 11:10:05 -0700

    Rewrite my custom git status for speeeed
    In my tests this cuts about a second off of the time to print the status
    of a repo with 9 changes. It went from 1.6 to 0.6 seconds--pretty
    significant!

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,5 +1,6 @@
 function git
     set -l translated_argv
+    # TODO: this won't detect commands with e.g. `git --git-dir ...`
     set -l cmd $argv[1]
     if math (count $argv) '>' 1 >/dev/null
         set argv $argv[2..-1]
@@ -10,7 +11,7 @@ function git
     for arg in $argv
         if begin
             # is $arg a number, and small enough to be an index of $c?
-            echo $arg | grep '^\d*$' >/dev/null ^&1
+            echo $arg | grep -E '^\d+$' >/dev/null ^&1
             and math $arg '<=' (count $c) >/dev/null
         end
             set translated_argv $translated_argv $c[$arg]
@@ -20,96 +21,143 @@ function git
         end
     end
 
-    function __print_formatted_status
-        set -l detect_label '^\s+[a-z ]*?:\s+'
-        set -l status_output (command git status $translated_argv)
-        set -l git_status_code $status
-        # I'm gonna try and make this a state-machine.
-        set -l state starting
-        for line in $status_output
-            switch $line
-            case '  (use "*' 'no changes added*' # Ignore these lines outright
-            case 'On branch*' '' 'Your branch is*' # Passthroughs
-                echo $line
-            case 'Changes to be committed*' # state-change indicator
-                echo $line
-                set state staged
-            case 'Changes not staged for commit*' # state-change indicator
-                echo $line
-                set state unstaged
-            case 'Untracked files*' # state-change indicator
-                echo $line
-                set state untracked
-            case '*' # inspect state and do some magic
-                # split the line into the label ("deleted", "modified",
-                # etc.) and the filename. Untracked files don't have a label,
-                # so just use the leading whitespace.
-                set -l label
-                set -l filename
-                switch $state
-                case staged unstaged
-                    set label (echo $line | ack -o $detect_label)
-                    set filename (echo $line | sed -E 's/'$detect_label'//')
-                case untracked
-                    set label (echo $line | ack -o '^\s+')
-                    set filename (echo $line | sed -E 's/^\s+//')
-                end
-
-                # determine the color to use for displaying this line's
-                # filename(s). I could do something fancy here, with asking
-                # git about its status output, but instead I'm just hardcoding
-                # my own preferences. (•_•) ( •_•)>⌐■-■ (⌐■_■)
-                set -l file_color
-                switch $state
-                case staged
-                    set file_color green
-                case unstaged
-                    set file_color yellow
-                case untracked
-                    set file_color red
-                end
-
-                switch $label
-                case '*renamed:*'
-                    # renamed files need additional processing, to put an
-                    # index next to both the old and new filenames. The
-                    # processing here will break in the inappropriate case
-                    # that either of the names contains ' -> '.
-                    set -l old (echo $filename | sed 's/ -> .*//')
-                    set -l new (echo $filename | sed 's/.* -> //')
-                    echo -n $label'['(contains -i $old $c)'] '
-                    set_color $file_color
-                    echo -n $old
-                    set_color normal
-                    echo -n ' -> ['(contains -i $new $c)'] '
-                    set_color $file_color
-                    echo -n $new
-                    set_color normal
-                    echo ''
-                case '*'
-                    # The label contains all the necessary whitespace/padding.
-                    # `contains -i` prints the index at which the given value
-                    # can be found in the given list.
-                    echo -n $label'['(contains -i $filename $c)'] '
-                    set_color $file_color
-                    echo -n $filename
-                    set_color normal
-                    echo ''
-                end
-            end
-        end
-
-        return $git_status_code
-    end
-
     switch $cmd
     case 'status'
         acquire_git_changes
-        __print_formatted_status
+        _git_status $translated_argv
         return $status
     case '*'
         command git $cmd $translated_argv
         return $status
     end
+end
 
+function _git_status
+    # TODO: what if some format other than --short is here?
+    set -l short_status (command git status --short $argv)
+    set -l branch (git symbolic-ref HEAD ^/dev/null | sed 's:refs/heads/::')
+    set -l staged_lines
+    set -l unstaged_lines
+    set -l unknown_lines
+    for line in $short_status
+        set -l staged_state (echo $line | sed 's/^\(.\).*/\1/')
+        set -l unstaged_state (echo $line | sed 's/^.\(.\).*/\1/')
+        switch $staged_state
+        case '\?' ' '
+        case '*'
+            set staged_lines $staged_lines $line
+        end
+
+        switch $unstaged_state
+        case ' '
+        case '\?'
+            set unknown_lines $unknown_lines $line
+        case '*'
+            set unstaged_lines $unstaged_lines $line
+        end
+    end
+
+    echo On branch (set_color --bold)$branch(set_color normal)
+    echo
+
+    if [ (count $staged_lines) != 0 ]
+        echo (set_color --bold green)Changes to be committed:(set_color normal)
+        for line in $staged_lines
+            _staged_status_line $line
+        end
+        echo
+    end
+
+    if [ (count $unstaged_lines) != 0 ]
+        echo (set_color --bold yellow)Unstaged changes:(set_color normal)
+        for line in $unstaged_lines
+            _unstaged_status_line $line
+        end
+        echo
+    end
+
+    if [ (count $unknown_lines) != 0 ]
+        echo (set_color --bold red)Untracked files:(set_color normal)
+        for line in $unknown_lines
+            _unknown_status_line $line
+        end
+        echo
+    end
+end
+
+function _staged_status_line
+    set -l line $argv[1]
+    set -l filename (echo $line | sed 's/...//')
+    set -l state (echo $line | sed 's/^\(.\).*/\1/')
+
+    _padding green
+    switch $state
+    case M; echo -n '  modified'
+    case A; echo -n '  new file'
+    case D; echo -n '   deleted'
+    case R; echo -n '   renamed'
+    case C; echo -n '    copied'
+    case T; echo -n 'typechange'
+    case ' ' '\?'
+    case '*' # unknown state!?
+        echo -n '         '$state
+    end
+
+    _display_filename $filename green
+end
+
+function _unstaged_status_line
+    set -l line $argv[1]
+    set -l filename (echo $line | sed 's/...//')
+    set -l state (echo $line | sed 's/^.\(.\).*/\1/')
+
+    _padding yellow
+    switch $state
+    case M; echo -n '  modified'
+    case D; echo -n '   deleted'
+    case T; echo -n 'typechange'
+    case ' ' '\?'
+    case '*' # unknown state!
+    echo -n '         '$state
+    end
+
+    _display_filename $filename yellow
+end
+
+function _unknown_status_line
+    set -l line $argv[1]
+    set -l filename (echo $line | sed 's/...//')
+
+    _padding red
+    echo -n ' untracked'
+    _display_filename $filename red
+end
+
+function _display_filename
+    set -l filename $argv[1]
+    set -l file_color $argv[2]
+
+    switch $filename
+    case '* -> *'
+        set -l old (echo $filename | sed 's/ -> .*//')
+        set -l new (echo $filename | sed 's/.* -> //')
+        echo -n ': ['(contains -i $old $c)'] '
+        set_color $file_color
+        echo -n $old
+        set_color normal
+        echo -n ' -> ['(contains -i $new $c)'] '
+        set_color $file_color
+        echo -n $new
+    case '*'
+        echo -n ': ['(contains -i $filename $c)'] '
+        set_color $file_color
+        echo -n $filename
+    end
+    echo
+    set_color normal
+end
+
+function _padding
+    set -l color $argv[1]
+    echo -n (set_color --bold $color)'#'(set_color normal)'      '
 end
