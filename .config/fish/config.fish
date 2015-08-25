23f480c39525c7f5feb8096d3c1924b29f071bb9
Erin Call <hello@erincall.com>
2015-08-25 09:48:47 -0700

    Show a notification when long-running tasks finish
    Inspired by https://gist.github.com/abacon/cb89d8f97cbeebf4924b

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -60,6 +60,39 @@ alias gc 'git commit'
 alias ga 'git add'
 alias gap 'git add --patch'
 
+function set_start_time \
+         --on-event fish_preexec \
+         --description 'note the epoch-time when each command started'
+    set -g command_started (date +%s)
+end
+
+function potentially_notify \
+         --on-event fish_postexec \
+         --description 'display a notification when a command finishes, if it took more than 20 seconds to run'
+    set -l last_status $status
+    set -l command_ended (date +%s)
+
+    set -l good_messages 😏 👏 🙌 😘 👻 🎉 🙏 😎 🍩 👌 🌟 🎊 ✨
+    set -l bad_messages 😤 😒 😧 💔 🔥 ❌ 😱 😰 😨 😩 😵 😡 😖
+    set -l message_icon
+
+    if math $command_ended - $command_started '>' 20 > /dev/null
+        if [ $last_status = 0 ]
+            set message_icon (_select_one $good_messages)
+        else
+            set message_icon (_select_one $bad_messages)
+        end
+
+        terminal-notifier -title "$message_icon $argv[1]" \
+                          -message "`$argv` completed."
+    end
+end
+
+function _select_one
+    set idx (math (random) '%' (count $argv) + 1)
+    echo $argv[$idx]
+end
+
 # GNU systems
 set -g -x LS_COLORS "no=00:fi=00:di=01;36:ln=01;36:pi=40;33:so=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=01;32:*.cmd=01;32:*.exe=01;32:*.com=01;32:*.btm=01;32:*.bat=01;32:*.sh=01;32:*.csh=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tz=01;31:*.rpm=01;31:*.cpio=01;31:*.jpg=01;35:*.gif=01;35:*.bmp=01;35:*.xbm=01;35:*.xpm=01;35:*.png=01;35:*.tif=01;35:"
 # BSD systems including Darwin
