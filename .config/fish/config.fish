c0868ff5ef830da2d9e7216e6b9e2837ab86843e
Erin Call <hello@erincall.com>
2017-10-30 16:33:08 -0700

    new gpg-agent setup for newer gpg
    gpg 2.1 no longer uses GPG_AGENT_INFO so there's no need for a
    .gpg-agent-info file. Note that gpg agent integration no longer works on
    computers that use an older verison of GPG.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,13 +16,7 @@ set -g -x GIT_AUTHOR_EMAIL 'hello@erincall.com'
 set -g -x GIT_COMMITTER_NAME $GIT_AUTHOR_NAME
 set -g -x GIT_COMMITTER_EMAIL $GIT_AUTHOR_EMAIL
 
-if not begin
-    [ -f ~/.gpg-agent-info ]
-    and kill -0 (cut -d: -f 2 ~/.gpg-agent-info) ^/dev/null
-end
-    gpg-agent --daemon --no-grab --write-env-file ~/.gpg-agent-info >/dev/null ^&1
-end
-set -g -x GPG_AGENT_INFO (cut -c 16- ~/.gpg-agent-info)
+gpg-agent --daemon --no-grab >/dev/null ^&1
 set -g -x GPG_TTY (tty)
 
 set -g -x NOSE_REDNOSE '1'
