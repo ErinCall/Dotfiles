03fae65bc1ccff13a21535fe3af2a1b1eebbf978
Erin Call <hello@erincall.com>
2018-01-22 13:53:32 -0800

    Use `cb` in grug
    The `git name-rev` approach had problems; it would sometimes return a
    tag and then resetting wouldn't work. `cb` uses `git symbolic-ref HEAD`
    which has been much more reliable.

--- .config/fish/functions/grug.fish
+++ .config/fish/functions/grug.fish
@@ -2,7 +2,7 @@ function grug
     set -l remote
     set -l branch
     set -q argv[1]; and set remote $argv[1]; or set remote origin
-    set -q argv[2]; and set branch $argv[2]; or set branch (git name-rev --name-only HEAD)
+    set -q argv[2]; and set branch $argv[2]; or set branch (cb)
 
     set -l remote_url (git remote -v | grep $remote | grep fetch | cut -f2 | sed 's/ .*//')
     echo "resetting to "$remote_url" at "$branch
