8d8ff8d934167a6f964addceb94f4178bc3ee79f
Erin Call <hello@erincall.com>
2019-11-13 13:47:27 -0800

    Support custom trunk branches in `glorm`
    If a repo uses something other than `master` as its trunk branch, dump
    the correct name in $REPO_DIR/.trunk and `glorm` will work as intended.
    
    `glord` is no longer necessary, since it was a special case of this
    feature.

--- .config/fish/functions/glord.fish
+++
@@ -1,7 +0,0 @@
-function glord
-    set -l master
-
-    set -q argv[1]; and set master $argv[1]; or set master development
-    set -q argv[2]; and set argv $argv[2:-1]
-    glorm $master $argv
-end
