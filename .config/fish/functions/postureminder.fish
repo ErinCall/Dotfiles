217f39eedeace16497e1d7bec4ed4be7364c3626
Erin Call <hello@erincall.com>
2019-12-03 09:10:31 -0800

    Function for reminding me to have good posture

---
+++ .config/fish/functions/postureminder.fish
@@ -0,0 +1,16 @@
+function postureminder
+    set -l sound
+
+    if test (count $argv) -gt 0
+        set sound -sound
+
+        switch $argv[1]
+        case "sound"
+            set sound $sound Blow
+        case '*'
+            set sound $sound $argv[1]
+        end
+    end
+
+    while true; terminal-notifier -title Posture -message 'spine straight, shoulders down' $sound; sleep (math 60 '*' 5); end
+end
