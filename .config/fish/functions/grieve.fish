c180d0997cadbb6a84b0d670e29b64d748c90996
Erin Call <hello@erincall.com>
2021-07-30 12:59:41 -0700

    Add glorm's custom-trunk feature to grieve

--- .config/fish/functions/grieve.fish
+++ .config/fish/functions/grieve.fish
@@ -1,10 +1,19 @@
 function grieve
-    set -l master
+    set -l trunk
     set -l topic
+    set -l default_trunk
 
-    set -q argv[1]; and set master $argv[1]; or set master 'master'
-    set -q argv[2]; and set topic $argv[2]; or set topic 'HEAD'
+    set -l trunk_file (git rev-parse --show-toplevel)/.trunk
+
+    if test -r $trunk_file
+        set default_trunk (cat $trunk_file)
+    else
+        set default_trunk master
+    end
+
+    set -q argv[1]; and set trunk $argv[1]; or set trunk $default_trunk
+    set -q argv[2]; and set topic $argv[2]; or set topic HEAD
     set -q argv[3]; and set argv $argv[3..-1]; or set argv
 
-    git log --color --patch --reverse --stat --no-prefix $master..$topic $argv | less --pattern '^commit|^diff'
+    git log --color --patch --reverse --stat --no-prefix $trunk..$topic $argv | less --pattern '^commit|^diff'
 end
